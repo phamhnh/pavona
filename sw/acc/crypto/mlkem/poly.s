56210907dcf6cca8dd41bcac62e7fa274f1d4068
Pham Hoang Nguyen Hien <nguyenhien.phamhoang@gmail.com>
2026-09-04 13:53:48 +0200

    [acc,mlkem] Add `mod` to list of input parameters of related functions.
    Signed-off-by: Pham Hoang Nguyen Hien <nguyenhien.phamhoang@gmail.com>

--- sw/acc/crypto/mlkem/poly.s
+++ sw/acc/crypto/mlkem/poly.s
@@ -183,6 +183,7 @@ poly_sub:
  * @param[in]     w16 (sw0): sw0.0 = q = 3329 (1st 16-bit lane),
  *                           sw0.2 = -q^-1 mod 2^16 = 3327 (3rd 16-bit lane)
  * @param[in]     w31: all-zero register
+ * @param[in]     mod: q = 3329
  *
  * clobbered registers: x4 to x5, x10, w0 to w1, acch, acc
  * clobbered flag groups: none
